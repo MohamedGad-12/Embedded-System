17aeddf2129fa92f6ab5750efa752d792b6b17e6
Mohamed Gamal <85452808+MMMooohaammeeddd@users.noreply.github.com>
2022-07-19 21:51:20 +0200

    unit 2

---
+++ unit2/lesson3/Swap.c
@@ -0,0 +1,35 @@
+/*
+ * main.c
+ *
+ *  Created on: ??‏/??‏/????
+ *      Author: *********
+ */
+
+#include "stdio.h"
+#include "stdlib.h"
+
+int main (int argc, char *argv){
+
+	int x = 0 ;
+	int y = 0 ;
+
+	printf ("Enter Two Number.\n");
+
+	fflush(stdin); fflush(stdout);
+
+	scanf("%d",&x);
+
+	scanf("%d",&y);
+
+	x = x + y;
+	y = x - y;
+	x = x - y;
+
+	printf ("%d", x);
+	fflush(stdin); fflush(stdout);
+	printf ("\n");
+	printf ("%d", y);
+
+	return 0;
+}
+
